def1c6a04eaf9314890337acd3cdeb6c3b2b7346
David Nanjila <nanjiladavid2@gmail.com>
2024-05-02 00:38:41 +0300

    feat: initializing the firebase

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyB49Wqkm_ZGFLn5e4gZ_bnh7jLI4j2-xM",
+    authDomain: "taskmanager-230ef.firebaseapp.com",
+    projectId: "taskmanager-230ef",
+    
+})
