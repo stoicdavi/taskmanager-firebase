35a17104a3364a055b24111e363d53158769f1b4
David Nanjila <nanjiladavid2@gmail.com>
2024-05-02 00:59:11 +0300

    feat: Create a function to add an item to the list

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Initialize Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyB49Wqkm_ZGFLn5e4gZ_bnh7jLI4j2-xM",
+    apiKey: "AIzaSyB49Wqkm_ZQGFLn5e4gZ_bnh7jLI4j2-xM",
     authDomain: "taskmanager-230ef.firebaseapp.com",
     projectId: "taskmanager-230ef",
     
 })
+const db = firebase.firestore();
+//function to add a task
+function addTask(){
+    const taskInput = document.getElementById('task_Input');
+    const task = taskInput.value.trim();
+    if (task !==''){
+        db.collection('tasks').add({
+            task: task,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        taskInput.value = '';
+    }
+}
+//
